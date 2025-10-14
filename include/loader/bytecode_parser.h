083e19ddd7fa6a7897753aa7f44390cbc236a86b
Lazy Cat <yagi.st123@gmail.com>
2025-10-14 19:59:17 +0700

    chore: add ideas

--- include/loader/bytecode_parser.h
+++ include/loader/bytecode_parser.h
@@ -6,17 +6,17 @@ class MemoryManager;
 
 class BytecodeParser {
 public:
-    std::unordered_map<Str, Proto> protos;
+    std::unordered_map<std::string, Proto> protos;
     BytecodeParser() = default;
-    Bool parseFile(const Str& filepath, MemoryManager& mm);
-    Bool parseSource(const Str& source, const Str& sourceName = "<string>");
+    Bool parseFile(const std::string& filepath, MemoryManager& mm);
+    Bool parseSource(const std::string& source, const std::string& sourceName = "<string>");
 private:
     MemoryManager* memoryManager = nullptr;
     Proto currentProto = nullptr;
-    Bool parseLine(const Str& line, const Str& sourceName, Int lineNumber);
-    Bool parseDirective(const std::vector<Str>& parts, const Str& sourceName, Int lineNumber);
-    Value parseConstValue(const Str& token);
-    std::vector<Str> split(const Str& s);
+    Bool parseLine(const std::string& line, const std::string& sourceName, Int lineNumber);
+    Bool parseDirective(const std::vector<std::string>& parts, const std::string& sourceName, Int lineNumber);
+    Value parseConstValue(const std::string& token);
+    std::vector<std::string> split(const std::string& s);
     void resolveAllLabels();
     void linkProtos();
 };
